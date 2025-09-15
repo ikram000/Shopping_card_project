6530c28f2b8aff5364f9857ae1ca8fa757ac50c1
ikram000 <ikramjaved857@gmail.com>
2025-09-15 00:20:28 -0700

    create store redux

---
+++ src/redux/store/index.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counter from '../counterSlice'
+
+export const store = configureStore({
+    reducer: {
+        counter: counter
+    }
+
+
+})
+
